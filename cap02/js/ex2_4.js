4602554190253eafebe3d83eadb83dca3ffc7750
antoniocarlosdiniz8 <57872835+antoniocarlosdiniz8@users.noreply.github.com>
2023-01-10 07:18:42 -0300

    Create ex2_4

---
+++ cap02/js/ex2_4.js
@@ -0,0 +1,4 @@
+const frm = document.querySelector("form")
+const kilo = document.querySelector("#iquilo")
+const grama = document.querySelector("iconsumo")
+const resul = document.querySelector("#mostrar")
