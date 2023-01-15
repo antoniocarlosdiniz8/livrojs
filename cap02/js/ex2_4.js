db376b839839d8c5cd1b9769e7cec4d86ffccc26
antoniocarlosdiniz8 <57872835+antoniocarlosdiniz8@users.noreply.github.com>
2023-01-15 06:11:59 -0300

    Atualizei ex2_4

--- cap02/js/ex2_4.js
+++ cap02/js/ex2_4.js
@@ -1,11 +1,17 @@
 const frm = document.querySelector("form")
-const kilo = document.querySelector("#iquilo")
-const grama = document.querySelector("#iconsumo")
-const resul = document.querySelector("#mostrar")
+//const kilo = document.querySelector("#iquilo")
+//const grama = document.querySelector("#iconsumo")
+const resul = document.querySelector("h3")
 
 
 frm.addEventListener("submit", (e) => {
-    
+    const quilo = document.querySelector(frm.iquilo.value)
+    const consumo = document.querySelector(frm.iconsumo.value)
+
+    const valor = (quilo / 1000) * consumo
+    resul.innerText = `Valor a pagar R$ ${valor.toFixed(2)}`
+
+    e.preventDefault()
 
 
 
