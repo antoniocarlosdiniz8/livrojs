f78d57144f1d264ec92bda30b3300f96b6637cf2
antoniocarlosdiniz8 <57872835+antoniocarlosdiniz8@users.noreply.github.com>
2023-01-22 16:11:06 -0300

    Update cap03

---
+++ cap03/node_3ad9f6892beea.tmp.js
@@ -0,0 +1,7 @@
+const prompt = require("prompt-sync")()
+const num1 = Number(prompt("1º Numero: "))
+const num2 = Number(prompt("2º Numero: "))
+
+const soma = num1 + num2
+
+console.log(`A soma é: ${soma}`)
